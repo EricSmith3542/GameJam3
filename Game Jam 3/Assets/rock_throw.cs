9dd83eb061cbb4794f71359ccd9dfc505fc472eb
jaredgreen2 <46983487+jaredgreen2@users.noreply.github.com>
2019-10-27 20:58:59 -0700

    rock throwing- no rocks, though
    the script rock_throw should be attached to all the rocks.
    also, it needs to be finished by you.

---
+++ Game Jam 3/Assets/rock_throw.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock_throw : MonoBehaviour
+{
+    gameObject player;
+    gameObject Worm;
+    float velocity;
+    float upVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()//lookat, press to collect
+        {
+            //place relative to camera
+            //parent to player
+        }
+        if()//press to throw
+        {
+            this.gameObject.Transform.Velocity = velocity*(player.transform.foreward) + upVelocity*(player.transform.up);
+        }
+    }
+    void onCollision()//ground
+    {
+        //set worm's destination to here
+    }
+}
