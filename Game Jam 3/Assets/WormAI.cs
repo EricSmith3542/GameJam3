56f252f6330b50bef693f7b0a22ab16ce3d865fa
Eric Smith <ericsmith3542@gmail.com>
2019-10-27 18:51:12 -0700

    WormAI Skeleton Code

--- Game Jam 3/Assets/WormAI.cs
+++ Game Jam 3/Assets/WormAI.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WormAI : MonoBehaviour
 {
+    private NavMeshAgent navMeshAgent;
+    private Animator anim;
+
+    public enum AIState { idle, chase, tremor, attack }
+
+    public AIState aiState = AIState.idle;
+
+    float maxStunDistance = 10;
+    public bool isLightOn = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Think());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    IEnumerator Think()
+    {
+        while (true)
+        {
+            switch (aiState)
+            {
+                case AIState.idle:
+                    break;
+                case AIState.chase:
+                    break;
+                case AIState.tremor:
+                    break;
+                case AIState.attack:
+                    break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
